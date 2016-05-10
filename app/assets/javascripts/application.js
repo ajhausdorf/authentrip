d4bcd9f3328e600693eff2a926b30bb7f0f8ac0f
ajhausdorf <ajhausdorf@wustl.edu>
2016-05-10 10:44:12 -0500

    second section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var tl = 0;
+var magnitude = 0.3;
+
+addEventListener('scroll', function(){
+    var top = $(document).scrollTop();
+    interp(top);
+});
+
+function interp(t){
+  var x = t-(tl-t);
+  console.log(x);
+  $('.header').css({top: x*magnitude});
+  $('.tekst').css({bottom: x*magnitude*1});
+  $('.tekst').css({bottom: x*magnitude});  
+}
